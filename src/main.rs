753dfa49f28d6173488aeb241f554816ce1d1ea3
dougfort <doug.fort@gmail.com>
2022-10-25 14:57:16 -0400

    show the  'ball' from the breakout example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Ball;
+
+// We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
+const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -50.0, 1.0);
+const BALL_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
+const BALL_SPEED: f32 = 400.0;
+const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
+const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+// Add the game's entities to our world
+fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn_bundle(Camera2dBundle::default());
+    // Ball
+    commands
+        .spawn()
+        .insert(Ball)
+        .insert_bundle(SpriteBundle {
+            transform: Transform {
+                scale: BALL_SIZE,
+                translation: BALL_STARTING_POSITION,
+                ..default()
+            },
+            sprite: Sprite {
+                color: BALL_COLOR,
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Velocity(INITIAL_BALL_DIRECTION.normalize() * BALL_SPEED));
 }
